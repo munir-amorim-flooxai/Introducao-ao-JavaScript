2a48d0f126c980fa08c57ed002bdcd341695e7eb
munir-amorim-flooxai <munir.amorim.flooxai@flooxai.com>
2025-02-24 12:35:30 -0300

    feat: create project of javascript introduction

---
+++ index.js
@@ -0,0 +1,15 @@
+/* var olaMundo = "Hello World!";
+
+console.log(OlaMundo);
+console.log(OlaMundo);
+console.log(OlaMundo);
+console.log(OlaMundo); */
+
+let n = 7;
+for (let i = 0; 1 <=10; i++){
+
+    console.log(`
+        ${i} X ${n} = {i*n}
+        `);
+        //console.log(i + " X " + n + " = " + (i * n));
+}
